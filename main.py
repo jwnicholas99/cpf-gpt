3457508c4ac1ae0b7b0b7d2c967c5e6cd29cf147
jwnicholas99 <jwnicholas99@gmail.com>
2023-03-12 23:32:12 +0800

    [FEAT] Use Streamlit to create front-end for app

--- main.py
+++ main.py
@@ -4,35 +4,52 @@ import streamlit as st
 
 from langchain import OpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.chains import VectorDBQAWithSourcesChain
 
-class CPF_QA():
-    def __init__(self, index_path):
-        embeddings = OpenAIEmbeddings()
-        self.vectorstore = Chroma(
-            embedding_function=embeddings, 
-            persist_directory=index_path
-        )
-        self.chain = VectorDBQAWithSourcesChain.from_chain_type(
-            OpenAI(model_name="gpt-3.5-turbo", temperature=0), 
-            chain_type="stuff", 
-            vectorstore=self.vectorstore,
-            return_source_documents=True
-        )
+@st.cache_resource
+def load_vectorstore(index_path):
+    embeddings = OpenAIEmbeddings()
+    vectorstore = Chroma(
+        embedding_function=embeddings, 
+        persist_directory=index_path
+    )
+    return vectorstore
+
+@st.cache_resource
+def load_chain(_vectorstore):
+    chain = VectorDBQAWithSourcesChain.from_chain_type(
+        OpenAI(model_name="gpt-3.5-turbo", temperature=0), 
+        chain_type="stuff", 
+        vectorstore=vectorstore,
+        return_source_documents=True
+    )
+    return chain
     
-    def search_docs(self, question):
-        return self.vectorstore.similarity_search(
-            question, k=4
-        )
+def search_docs(vectorstore, question):
+    return vectorstore.similarity_search(
+        question, k=4
+    )
 
-    def query(self, question):
-        # Source documents are stored in source_documents in response
-        return self.chain({"question": question}, return_only_outputs=True)
+def query(chain, question):
+    # Source documents are stored in source_documents in response
+    return chain({"question": question}, return_only_outputs=True)
 
-if __name__=='__main__':
-    load_dotenv()
-    agent = CPF_QA("./index")
-    #print(agent.search_docs("Can the CPF funds be used to pay for Medishield?"))
-    print(agent.query("What is the employer's CPF contribution for somebody who is 56 years old?"))
+# App
+load_dotenv()
+vectorstore = load_vectorstore("./index")
+chain = load_chain(vectorstore)
+
+st.header("`CPF-GPT`")
+st.info("`Hello! I am a ChatGPT connected to the Singapore CPF 1953 Act.`")
+question = st.text_input("`Please ask a question:` ")
+
+if question:
+    response = query(chain, question)
+
+    # Display answer
+    st.info("`%s`"%response['answer'])
+
+    # Display sources
+    for source_doc in response['source_documents']:
+        st.info("`%s`"%source_doc)
